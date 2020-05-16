2112b640f9e385e260d795309ee6cefcd4b35e8d
__patrick__ <krustenkaese@yandex.com>
2020-05-16 02:53:24 +0200

    * file only gets created if associated item is yielded * renamings

--- Pipelines/MultiItem/pipeline.py
+++ Pipelines/MultiItem/pipeline.py
@@ -2,18 +2,17 @@
 import os
 
 class MultiItemPipeline(object):
-    def __init__(self, output_directory, exporters, stats):
-        self.stats = stats
-        self.__output_directory = self.__create_output_directory(output_directory)
-        self.__exporter_settings = exporters
+    def __init__(self, crawler):
+        self.stats = crawler.stats
+        self.settings = crawler.settings
+        self.exporters = self.settings.get('EXPORTERS', {})
+        self.export_path = self.__create_directory(self.settings.get('OUTPUT_DIRECTORY', 'out'))
     
     @classmethod
     def from_crawler(cls, crawler):
-        output_directory = crawler.settings.get('OUTPUT_DIRECTORY', 'out')
-        exporters = crawler.settings.get('EXPORTERS', {})
-        return cls(output_directory, exporters, crawler.stats)
+        return cls(crawler)
 
-    def __create_output_directory(self, directory):
+    def __create_directory(self, directory):
         if not directory.endswith('/'):
             directory = directory + '/'
         if not os.path.exists(directory):
@@ -22,37 +21,45 @@ class MultiItemPipeline(object):
 
     def open_spider(self, spider):
         self.__exporter_for_item = {}
-        for exporter, values in self.__exporter_settings.items():
-            ex = self.__start_exporter(exporter, values['exporter'])
-            name = values['item'].__name__
-            self.stats.set_value(name, 0)
-            self.__exporter_for_item[name] = ex
 
-    def __start_exporter(self, name, exporter_class):
-        filename = self.__create_output_file(name)
-        exporter = exporter_class(open(filename, 'wb'))
+    def __start_exporter(self, filename, exporter_class):
+        file_path = self.__create_output_file(filename)
+        exporter = exporter_class(open(file_path, 'wb'))
         exporter.start_exporting()
+        self.stats.set_value(filename, 0)
         return exporter
 
     def __create_output_file(self, filename):
-        filepath = self.__output_directory + filename
+        file_path = self.export_path + filename
         for i in range(1, 999999):
-            if not os.path.exists(filepath + f'_{i}'):
-                filepath = filepath + f'_{i}'
-                f = open(filepath, 'w') 
+            if not os.path.exists(file_path + f'_{i}'):
+                file_path = file_path + f'_{i}'
+                f = open(file_path, 'w') 
                 f.close()
-                return filepath
+                return file_path
 
     def close_spider(self, spider):
         for exporter in self.__exporter_for_item.values():
             exporter.finish_exporting()
 
-    def _exporter_for_item(self, item):
-        item_class_name = item.__class__.__name__
-        self.stats.inc_value(item_class_name)
-        return self.__exporter_for_item[item_class_name]
+    def get_exporter_by_class_name(self, class_name):
+        for exporter in self.exporters:
+            if class_name in self.exporters[exporter]['item'].__name__:
+                return exporter, self.exporters[exporter]['exporter']
+
+    def __add_exporter(self, class_name):
+        if class_name not in self.__exporter_for_item:
+            filename, exporter_class = self.get_exporter_by_class_name(class_name)
+            exporter = self.__start_exporter(filename, exporter_class)
+            self.__exporter_for_item[class_name] = exporter
+
+    def get_exporter_for_item(self, item):
+        class_name = item.__class__.__name__
+        self.__add_exporter(class_name)
+        self.stats.inc_value(class_name)
+        return self.__exporter_for_item[class_name]
 
     def process_item(self, item, spider):
-        exporter = self._exporter_for_item(item)
+        exporter = self.get_exporter_for_item(item)
         exporter.export_item(item)
         return item
