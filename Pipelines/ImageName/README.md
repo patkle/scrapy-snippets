5df3fdc14adedfdfb61d60c50808dfeb52b24e03
__patrick__ <krustenkaese@yandex.com>
2020-05-18 22:02:59 +0200

    Extended ImageNamePipeline to accept different path-fields for each image field

--- Pipelines/ImageName/README.md
+++ Pipelines/ImageName/README.md
@@ -1,11 +1,11 @@
 # ImageNamePipeline
 ImageNamePipeline is a sub class of [ImagesPipeline](https://doc.scrapy.org/en/latest/_modules/scrapy/pipelines/images.html)
 
+## Settings
 To enable the pipeline in your project you need to set [ITEM_PIPELINES](https://docs.scrapy.org/en/latest/topics/item-pipeline.html#activating-an-item-pipeline-component) in [settings.py](https://docs.scrapy.org/en/latest/topics/settings.html)
 ```Python
 ITEM_PIPELINES = {
     'your_project.pipelines.ImageNamePipeline': 200,
-    
 }
 ```
 Also you need to define IMAGE_URL_FIELDS in settings.py as a Dictionary like this:
@@ -13,10 +13,19 @@ Also you need to define IMAGE_URL_FIELDS in settings.py as a Dictionary like thi
 IMAGE_URL_FIELDS = {
     'your_items_image_url_field': {
         'name_field': 'item_field_used_to_name_image',
-        'sub_folder': 'folder_to_which_file_gets_downloaded'
+        'sub_folder': 'folder_to_which_file_gets_downloaded',
+        'path_field': 'item_field_to_store_image_paths'
     }, 'second_image_url_field': {
         'name_field': 'name',
         'sub_folder': 'folder'
+        'path_field': 'path_field'
     }
 }
 ```
+
+## Additional information
+You cannot use ImagesPipelines [IMAGE_URLS_FIELD](https://docs.scrapy.org/en/latest/topics/media-pipeline.html?highlight=IMAGE_URLS_FIELD#std-setting-IMAGES_URLS_FIELD) with this sub class.
+However, you need to either implement IMAGES_RESULT_FIELD or provide the standard field *images* in your Item.
+
+## TODO
+- Add a working example
--- Pipelines/ImageName/pipeline.py
+++ Pipelines/ImageName/pipeline.py
@@ -9,20 +9,25 @@ class ImageNamePipeline(ImagesPipeline):
         self.image_url_fields = settings.get('IMAGE_URL_FIELDS', {})
 
     def get_media_requests(self, item, info):
-        for image_field_name, settings in self.image_url_fields.items():
+        for image_field_name, image_settings in self.image_url_fields.items():
             for i, image_url in enumerate(item.get(image_field_name, [])):
                 yield Request(
                     image_url, 
                     meta = {
-                        'image_name': item[settings.get('name_field', 'name')], 
-                        'image_folder': settings.get('sub_folder', ''),
+                        'image_name': item[image_settings.get('name_field', 'name')], 
+                        'image_folder': image_settings.get('sub_folder', ''),
                         'image_count': i
                     }
                 )
 
     def item_completed(self, results, item, info):
-        image_paths = [x['path'] for ok, x in results if ok]
-        item['image_paths'] = image_paths
+        results = [x for ok, x in results if ok]
+        for image_field_name, image_settings in self.image_url_fields.items():
+            image_path_list = []
+            for result in results:
+                if result['url'] in item[image_field_name]:
+                    image_path_list.append(result['path'])
+            item[image_settings['path_field']] = image_path_list
         return item
 
     def file_path(self, request, response=None, info=None):
