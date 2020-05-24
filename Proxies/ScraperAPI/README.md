37256e3282eebbd4e84e140dd8c890325a55767d
__patrick__ <krustenkaese@yandex.com>
2020-05-24 19:41:27 +0200

    Added support for paid options in Scraper API

--- Proxies/ScraperAPI/README.md
+++ Proxies/ScraperAPI/README.md
@@ -14,5 +14,17 @@ DOWNLOADER_MIDDLEWARES = {
 }
 ```
 
+## Additional options 
+Scraper API supports additional options as documented [here](https://www.scraperapi.com/documentation?fp_ref=patrick50#proxy-mode).
+You could use these options by adding them to your settings as dictionary.
+```python
+SCRAPER_API_OPTIONS = {
+    'render': 'true', 
+    'country_code': 'us'
+}
+```
+However, this feature is untested since I am only on a free plan. Also, make sure to pass the values for these options as strings.
+If you encounter any errors with this, please let me know. 
+
 ## Affiliate link
 If this example is helpful to you and you do not yet have a subscription to Scraper API, consider using [my affiliate link](https://www.scraperapi.com/pricing?fp_ref=patrick50) if you plan on getting one.
--- Proxies/ScraperAPI/middleware.py
+++ Proxies/ScraperAPI/middleware.py
@@ -3,10 +3,18 @@
 class ScraperAPIMiddleware(object):
     def __init__(self, crawler):
         self.key = crawler.settings.get('SCRAPER_API_KEY')
+        self.user = self.__get_user(crawler.settings.get('SCRAPER_API_OPTIONS'))
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler)
 
+    def __get_user(self, options):
+        user = 'scraperapi'
+        if options is not None and isinstance(options, dict):
+            for key, value in options.items():
+                user = f'{user}.{key}={value}'
+        return user
+
     def process_request(self, request, spider):
-        request.meta['proxy'] = f'http://scraperapi:{self.key}@proxy-server.scraperapi.com:8001'
+        request.meta['proxy'] = f'http://{self.user}:{self.key}@proxy-server.scraperapi.com:8001'
