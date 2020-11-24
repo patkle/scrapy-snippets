641f24c4a67a4c602d7252c77a663bb246a97aad
__patrick__ <krustenkaese@yandex.com>
2020-11-24 22:32:55 +0100

    Added comments Minor refactoring

--- Pipelines/MultiItem/pipeline.py
+++ Pipelines/MultiItem/pipeline.py
@@ -19,6 +19,11 @@ class MultiItemPipeline(object):
         self.__export_item(item)
         return item
 
+    def close_spider(self, spider):
+        # stop all item exporters when spider finishes 
+        for exporter in self._exporter_for_item.values():
+            exporter.finish_exporting()
+
     def __set_output_directory(self, directory: str) -> None:
         """create output directory if it does not exist yet"""
         # ensure that directory path ends with a slash
@@ -30,38 +35,41 @@ class MultiItemPipeline(object):
             os.mkdir(directory)
         self.output_directory = directory
 
-    def __create_output_file(self, filename: str):
-        file_path = self.output_directory + filename
-        for i in range(1, 999999):
-            if not os.path.exists(file_path + f'_{i}'):
-                file_path = f'{file_path}_{i}'
-                open(file_path, 'w').close()
-                return file_path
-
-    def close_spider(self, spider):
-        # close all item exporters when spider finishes 
-        for exporter in self._exporter_for_item.values():
-            exporter.finish_exporting()
-
-    def __export_item(self, item):
+    def __export_item(self, item) -> None:
         """export item with exporter according to settings"""
         class_name = item.__class__.__name__
-        exporter = self._exporter_for_item.get(class_name, None)
-        if exporter is None:
-            exporter = self.__start_exporter(class_name)
+        exporter = self.__get_exporter_for_item_class(class_name)
         self.stats.inc_value(class_name)
         exporter.export_item(item)
 
-    def __start_exporter(self, class_name):
+    def __get_exporter_for_item_class(self, class_name):
+        exporter = self._exporter_for_item.get(class_name, None)
+        # create and start exporter, if it does not already exist
+        if exporter is None:
+            exporter = self.__start_exporter(class_name)
+        return exporter
+
+    def __start_exporter(self, class_name: str):
         filename, exporter_class = self.__get_exporter_class_by_item_class(class_name)
         file_path = self.__create_output_file(filename)
         exporter = exporter_class(open(file_path, 'wb'))
         exporter.start_exporting()
         self._exporter_for_item[class_name] = exporter
-        self.stats.set_value(filename, 0)
+        self.stats.set_value(class_name, 0)
         return exporter
 
     def __get_exporter_class_by_item_class(self, class_name: str):
         for filename in self.exporters:
             if class_name in self.exporters[filename]['item'].__name__:
                 return filename, self.exporters[filename]['exporter']
+
+    def __create_output_file(self, filename: str):
+        """creates a new output file with an appending number to ensure no existing file gets overwritten"""
+        file_path = self.output_directory + filename
+        for i in range(1, 999999):
+            # check if file does not yet exist
+            if not os.path.exists(f'{file_path}_{i}'):
+                file_path = f'{file_path}_{i}'
+                open(file_path, 'w').close()
+                return file_path
+
