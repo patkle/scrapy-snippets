822af7b21322e8b12586fc10eb7ba94bdc3f1632
__patrick__ <krustenkaese@yandex.com>
2020-11-25 17:21:50 +0100

    Improved example

--- Pipelines/ImageName/example.py
+++ Pipelines/ImageName/example.py
@@ -1,28 +1,35 @@
-# -*- coding: utf-8 -*-
 import pathlib
 import scrapy
 from scrapy.settings import Settings
 from scrapy.crawler import CrawlerRunner
 from twisted.internet import reactor
 
-class DQItem(scrapy.Item):
+
+class CatItem(scrapy.Item):
     title = scrapy.Field()
-    logo = scrapy.Field()
-    logo_path = scrapy.Field()
-    slime = scrapy.Field()
-    slime_path = scrapy.Field()
+    white = scrapy.Field()
+    white_path = scrapy.Field()
+    black = scrapy.Field()
+    black_path = scrapy.Field()
     images = scrapy.Field()
 
-class DQSpider(scrapy.Spider):
-    name = 'dq'
-    start_urls = ['https://en.wikipedia.org/wiki/Dragon_Quest']
+
+class CatSpider(scrapy.Spider):
+    name = 'cats'
+    start_urls = ['https://commons.wikimedia.org/wiki/Category:Domestic_cats']
 
     def parse(self, response):
-        title = response.xpath('.//h1/i/text()').extract_first()
-        logo = 'https://upload.wikimedia.org/wikipedia/en/5/56/Dragon_quest_logo.png'
-        slime = 'https://upload.wikimedia.org/wikipedia/en/1/13/Slime_%28Dragon_Quest%29.jpg'
-        print(DQItem(title=title, logo=logo, slime=slime))
-        yield DQItem(title=title, logo=logo, slime=slime)
+        title = response.xpath('.//h1/text()').get().replace('Category:', '')
+        white = [
+            'https://upload.wikimedia.org/wikipedia/commons/e/e5/Cat_mouse_1.jpg',
+            'https://upload.wikimedia.org/wikipedia/commons/7/72/Cat_In_My_Garden.jpg'
+        ]
+        black = [
+            'https://upload.wikimedia.org/wikipedia/commons/e/e2/Black_Cat_Nala.jpg',
+            'https://upload.wikimedia.org/wikipedia/commons/2/28/Benson_-_Outside_%28wgtn%2C_churtprk%29.jpg',
+            'https://upload.wikimedia.org/wikipedia/commons/e/ef/Ahhh-sunlight_2_%2815447982169%29.jpg'
+        ]
+        yield CatItem(title=title, white=white, black=black)
 
 def get_settings() -> Settings:
     """create and return a scrapy settings object"""
@@ -32,14 +39,14 @@ def get_settings() -> Settings:
     # set path in which to store images
     settings.set('IMAGES_STORE', cwd + '\\')
     settings.set('IMAGE_URL_FIELDS', {
-        'logo': {
+        'white': {
             'name_field': 'title',
-            'sub_folder': 'logos',
-            'path_field': 'logo_path',
-        }, 'slime': {
+            'sub_folder': 'white',
+            'path_field': 'white_path',
+        }, 'black': {
             'name_field': 'title',
-            'sub_folder': 'slimes',
-            'path_field': 'slime_path',
+            'sub_folder': 'black',
+            'path_field': 'black_path',
         }
     })
     # enable the pipeline
@@ -53,6 +60,6 @@ if __name__ == '__main__':
     # see: https://docs.scrapy.org/en/latest/topics/practices.html#run-scrapy-from-a-script
     settings = get_settings()
     runner = CrawlerRunner(settings)
-    d = runner.crawl(DQSpider)
+    d = runner.crawl(CatSpider)
     d.addBoth(lambda _: reactor.stop())
     reactor.run()
