649a7799f2a3a7b52c50f89636870ccbd07e5ff3
__patrick__ <krustenkaese@yandex.com>
2020-11-25 01:07:11 +0100

    Added comments Minor refactoring

--- Pipelines/MultiItem/example.py
+++ Pipelines/MultiItem/example.py
@@ -5,12 +5,14 @@ from scrapy.exporters import CsvItemExporter, JsonItemExporter
 from scrapy.crawler import CrawlerRunner
 from twisted.internet import reactor
 
+
 class CSVItem(scrapy.Item):
     quote = scrapy.Field()
+
+
+class JSONItem(scrapy.Item):
     author = scrapy.Field()
 
-class JSONItem(CSVItem):
-    pass
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
@@ -20,22 +22,29 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for container in response.xpath('//div[@class="quote"]'):
-            quote = container.xpath('.//span[@class="text"]/text()').extract_first()
-            author = container.xpath('.//small[@class="author"]/text()').extract_first()
-            print(author)
-            yield CSVItem(quote=quote, author=author)
-            yield JSONItem(quote=quote, author=author)
+        for quote in response.xpath('//div[@class="quote"]'):
+            yield CSVItem(quote=quote.xpath('.//span[@class="text"]/text()').get())
+            yield JSONItem(author=quote.xpath('.//small[@class="author"]/text()').get())
 
-def get_settings():
+
+def get_settings() -> Settings:
+    """Get scrapy settings"""
     settings = Settings()
+    # set directory in which output files will be saved
     settings.set('OUTPUT_DIRECTORY', 'export')
     settings.set('EXPORTERS', {
+        # key = filename
         'csv_quotes': {
+            # item class which to export to csv_quotes 
             'item': CSVItem,
+            # use the CsvItemExporter to export to csv
             'exporter': CsvItemExporter
-        }, 'json_quotes': {
+        }, 
+        # filename for the second file
+        'json_quotes': {
+            # JSONItems get saved to file json_quotes
             'item': JSONItem,
+            # use the JsonItemExporter to export to json
             'exporter': JsonItemExporter
         }
     })
@@ -45,6 +54,8 @@ def get_settings():
     return settings
 
 if __name__ == '__main__':
+    # routine to run scrapy from a script
+    # see: https://docs.scrapy.org/en/latest/topics/practices.html#run-scrapy-from-a-script
     settings = get_settings()
     runner = CrawlerRunner(settings)
     d = runner.crawl(QuotesSpider)
